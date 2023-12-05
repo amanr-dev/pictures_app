9376c9277c92cbc77977d1289347911fd6bf38b5
Aman <dv451197@gmail.com>
2023-12-05 20:45:20 +0530

    added cursor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Login from "./components/Login";
 import "./index.css";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { fetchUser } from "./utils/fetchUser";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   const navigate = useNavigate();
@@ -18,6 +19,27 @@ function App() {
 
   return (
     <>
+      <AnimatedCursor
+        innerSize={12}
+        outerSize={8}
+        color="0, 122, 252"
+        outerAlpha={0.8}
+        innerScale={0.7}
+        clickables={[
+          "a",
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          "label[for]",
+          "select",
+          "textarea",
+          "button",
+          ".link",
+          "img",
+        ]}
+      />
       <Routes>
         <Route path="login" element={<Login />} />
         <Route path="/*" element={<Home />} />
