68a777e6fb73a07c91ecae3064ab24a55a77b4b2
Aman <dv451197@gmail.com>
2023-12-22 15:26:33 +0530

    removed cursor animation in mobile devices

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import "./index.css";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { fetchUser } from "./utils/fetchUser";
 import AnimatedCursor from "react-animated-cursor";
+import { useMediaQuery } from "react-responsive";
 
 function App() {
   const navigate = useNavigate();
@@ -16,29 +17,37 @@ function App() {
     }
   }, []);
 
+  const responsive = useMediaQuery({
+    query: "(max-width: 666px)",
+  });
+
+  // console.log(responsive);
+
   return (
     <>
-      <AnimatedCursor
-        innerSize={12}
-        outerSize={8}
-        color="0, 122, 252"
-        outerAlpha={0.8}
-        innerScale={0.7}
-        clickables={[
-          "a",
-          'input[type="text"]',
-          'input[type="email"]',
-          'input[type="number"]',
-          'input[type="submit"]',
-          'input[type="image"]',
-          "label[for]",
-          "select",
-          "textarea",
-          "button",
-          ".link",
-          "img",
-        ]}
-      />
+      {!responsive && (
+        <AnimatedCursor
+          innerSize={12}
+          outerSize={8}
+          color="0, 122, 252"
+          outerAlpha={0.8}
+          innerScale={0.7}
+          clickables={[
+            "a",
+            'input[type="text"]',
+            'input[type="email"]',
+            'input[type="number"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            "label[for]",
+            "select",
+            "textarea",
+            "button",
+            ".link",
+            "img",
+          ]}
+        />
+      )}
       <Routes>
         <Route path="login" element={<Login />} />
         <Route path="/*" element={<Home />} />
