a29adb197abae1a80daa5054ba317c2b28fb68d4
Aman <dv451197@gmail.com>
2024-03-28 12:52:42 +0530

    style text

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -5,7 +5,9 @@ const Spinner = ({ text }) => {
   return (
     <div className="flex flex-col justify-center items-center w-full min-h-screen">
       <RotatingSquare color="#0260FF" height={80} width={200} className="m-5" />
-      <p className="text-lg text-slate-500 text-center px-2">{text}</p>
+      <p className="text-lg text-slate-500 text-center dark:text-slate-50 px-2">
+        {text}
+      </p>
     </div>
   );
 };
