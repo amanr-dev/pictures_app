30f1e9f8b83e8389759aba838b44139341c275cf
Aman <dv451197@gmail.com>
2024-08-09 22:01:40 +0530

    clean code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,6 @@ import Login from "./components/Login";
 import "./index.css";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { fetchUser } from "./utils/fetchUser";
-// import AnimatedCursor from "react-animated-cursor";
-// import { useMediaQuery } from "react-responsive";
 
 function App() {
   const navigate = useNavigate();
@@ -17,39 +15,8 @@ function App() {
     }
   }, [navigate]);
 
-  // const responsive = useMediaQuery({
-  //   query: "(max-width: 666px)",
-  // });
-
-  // console.log(responsive);
-
   return (
     <>
-      {/* Animated Cursor */}
-
-      {/* {!responsive && (
-        <AnimatedCursor
-          innerSize={12}
-          outerSize={8}
-          color="0, 122, 252"
-          outerAlpha={0.8}
-          innerScale={0.7}
-          clickables={[
-            "a",
-            'input[type="text"]',
-            'input[type="email"]',
-            'input[type="number"]',
-            'input[type="submit"]',
-            'input[type="image"]',
-            "label[for]",
-            "select",
-            "textarea",
-            "button",
-            ".link",
-            "img",
-          ]}
-        />
-      )} */}
       <Routes>
         <Route path="login" element={<Login />} />
         <Route path="/*" element={<Home />} />
